4b199a66f88c0ad4c85693beb5dead7adc2ef13f
dhoko <aurelien@protonmail.com>
2017-12-05 17:22:23 +0100

    Run upgrade style once

--- jquery.mCustomScrollbar.js
+++ jquery.mCustomScrollbar.js
@@ -1922,10 +1922,11 @@ and dependencies (minified).
 			}
 		},
 		/* -------------------- */
-
+		_isLoadedUPD = false,
 
 		/* calls the update method automatically */
 		_autoUpdate=function(rem){
+			var self = this;
 			var $this=$(this),d=$this.data(pluginPfx),o=d.opt,
 				mCSB_container=$("#mCSB_"+d.idx+"_container");
 			if(rem){
@@ -1937,49 +1938,53 @@ and dependencies (minified).
 				_delete(mCSB_container[0],"autoUpdate");
 				return;
 			}
-			// upd();
-			// function upd(){
-			// 	clearTimeout(mCSB_container[0].autoUpdate);
-			// 	if($this.parents("html").length===0){
-			// 		/* check element in dom tree */
-			// 		$this=null;
-			// 		return;
-			// 	}
-			// 	mCSB_container[0].autoUpdate=setTimeout(function(){
-			// 		/* update on specific selector(s) length and size change */
-			// 		if(o.advanced.updateOnSelectorChange){
-			// 			d.poll.change.n=sizesSum();
-			// 			if(d.poll.change.n!==d.poll.change.o){
-			// 				d.poll.change.o=d.poll.change.n;
-			// 				doUpd(3);
-			// 				return;
-			// 			}
-			// 		}
-			// 		/* update on main element and scrollbar size changes */
-			// 		if(o.advanced.updateOnContentResize){
-			// 			d.poll.size.n=$this[0].scrollHeight+$this[0].scrollWidth+mCSB_container[0].offsetHeight+$this[0].offsetHeight+$this[0].offsetWidth;
-			// 			if(d.poll.size.n!==d.poll.size.o){
-			// 				d.poll.size.o=d.poll.size.n;
-			// 				doUpd(1);
-			// 				return;
-			// 			}
-			// 		}
-			// 		/* update on image load */
-			// 		if(o.advanced.updateOnImageLoad){
-			// 			if(!(o.advanced.updateOnImageLoad==="auto" && o.axis==="y")){ //by default, it doesn't run on vertical content
-			// 				d.poll.img.n=mCSB_container.find("img").length;
-			// 				if(d.poll.img.n!==d.poll.img.o){
-			// 					d.poll.img.o=d.poll.img.n;
-			// 					mCSB_container.find("img").each(function(){
-			// 						imgLoader(this);
-			// 					});
-			// 					return;
-			// 				}
-			// 			}
-			// 		}
-			// 		if(o.advanced.updateOnSelectorChange || o.advanced.updateOnContentResize || o.advanced.updateOnImageLoad){upd();}
-			// 	},o.advanced.autoUpdateTimeout);
-			// }
+			upd();
+			function upd(){
+				if (self._isLoadedUPD) {
+				    return;
+				}
+
+				// clearTimeout(mCSB_container[0].autoUpdate);
+				if($this.parents("html").length===0){
+					/* check element in dom tree */
+					$this=null;
+					return;
+				}
+				// mCSB_container[0].autoUpdate=setTimeout(function(){
+					/* update on specific selector(s) length and size change */
+					if(o.advanced.updateOnSelectorChange){
+						d.poll.change.n=sizesSum();
+						if(d.poll.change.n!==d.poll.change.o){
+							d.poll.change.o=d.poll.change.n;
+							doUpd(3);
+							return;
+						}
+					}
+					/* update on main element and scrollbar size changes */
+					if(o.advanced.updateOnContentResize){
+						d.poll.size.n=$this[0].scrollHeight+$this[0].scrollWidth+mCSB_container[0].offsetHeight+$this[0].offsetHeight+$this[0].offsetWidth;
+						if(d.poll.size.n!==d.poll.size.o){
+							d.poll.size.o=d.poll.size.n;
+							doUpd(1);
+							return;
+						}
+					}
+					/* update on image load */
+					if(o.advanced.updateOnImageLoad){
+						if(!(o.advanced.updateOnImageLoad==="auto" && o.axis==="y")){ //by default, it doesn't run on vertical content
+							d.poll.img.n=mCSB_container.find("img").length;
+							if(d.poll.img.n!==d.poll.img.o){
+								d.poll.img.o=d.poll.img.n;
+								mCSB_container.find("img").each(function(){
+									imgLoader(this);
+								});
+								return;
+							}
+						}
+					}
+					// if(o.advanced.updateOnSelectorChange || o.advanced.updateOnContentResize || o.advanced.updateOnImageLoad){upd();}
+				// },o.advanced.autoUpdateTimeout);
+			}
 			/* a tiny image loader */
 			function imgLoader(el){
 				if($(el).hasClass(classes[2])){doUpd(); return;}
